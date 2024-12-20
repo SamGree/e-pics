8c1c661354a8baaac79364f424cd34934f4fbe9d
Sam Green <sam.green7777@yahoo.com>
2024-12-20 20:42:59 +0000

    start project and added users app

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
+
+# Create your models here.
+class User(AbstractUser):
+    """
+    Custom User model that extends the default AbstractUser model.
+    Includes additional fields for profile image, unique email, and user bio.
+    """
+    # Profile image stored using Cloudinary
+    profile_image = CloudinaryField('profile_image', blank=True, null=True)
+    # Optional bio field for user profiles
+    bio = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.username
