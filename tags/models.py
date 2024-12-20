6b8851722168ebb64bfbc96a1009cc360d4c5dc9
Sam Green <sam.green7777@yahoo.com>
2024-12-20 23:00:19 +0000

    create tags app

---
+++ tags/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Tag(models.Model):
+    """
+    Model representing a tag that can be associated with posts.
+    """
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
