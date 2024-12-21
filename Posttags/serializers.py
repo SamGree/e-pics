0d68fbb43a7db784555347a3a7c72fdac3eabfae
Sam Green <sam.green7777@yahoo.com>
2024-12-21 17:50:16 +0000

    add serializers to posttags app

---
+++ Posttags/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import PostTag
+
+class PostTagSerializer(serializers.ModelSerializer):
+    tag_name = serializers.CharField(source='tag.name', read_only=True)
+
+    class Meta:
+        model = PostTag
+        fields = ['id', 'post', 'tag', 'tag_name', 'created_at']
+        read_only_fields = ['id', 'created_at']
