0ad00b89a68d99fd6bcebc0b7d0a5b5d185612bf
Sam Green <sam.green7777@yahoo.com>
2024-12-22 20:43:08 +0000

    add postgresql to env file

--- e_pics/settings.py
+++ e_pics/settings.py
@@ -55,8 +55,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
-    'rest_framework.authtoken',
     'django_filters',
+    'rest_framework.authtoken', 
+    'dj_rest_auth',
+
     'corsheaders',    
     
     'posts',
