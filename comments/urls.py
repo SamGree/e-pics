44809ca996182bf8fcbc8f9671a2a56b75c3c883
SamGree <sam.green7777@yahoo.com>
2025-02-09 21:12:36 +0100

    Corrected urls for comments app

--- comments/urls.py
+++ comments/urls.py
@@ -8,7 +8,7 @@ from django.urls import path
 from .views import CommentCreateView, CommentUpdateView, PostCommentsView
 
 urlpatterns = [
-    path('<int:post_id>/post/', CommentCreateView.as_view(), name='create-comment'),  # Create a comment for a specific post
-    path('<int:comment_id>/', CommentUpdateView.as_view(), name='comment-detail'),  # Update or delete a specific comment
-    path('post/<int:post_id>/', PostCommentsView.as_view(), name='post-comments'),
+    path('<int:post_id>/post', CommentCreateView.as_view(), name='create-comment'),  # Create a comment for a specific post
+    path('<int:comment_id>', CommentUpdateView.as_view(), name='comment-detail'),  # Update or delete a specific comment
+    path('post/<int:post_id>', PostCommentsView.as_view(), name='post-comments'),
 ]
