a47fbf123d93cb7b6714ec130dad4b85c1da2389
SamGree <sam.green7777@yahoo.com>
2025-02-11 20:39:24 +0100

    fixed posts models file

--- posts/models.py
+++ posts/models.py
@@ -4,10 +4,9 @@ from users.models import User
 from django.conf import settings
 from cloudinary.models import CloudinaryField
 
-
 class Post(models.Model):
     """
-    Model representing a blog post, including title, description,
+    Model representing a blog post, including title, description, 
     associated user, and related likes.
     """
     title = models.CharField(max_length=100)
@@ -15,10 +14,8 @@ class Post(models.Model):
     image = CloudinaryField('image')  # Image uploaded via Cloudinary
     download_count = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(
-        User, related_name='posts', on_delete=models.CASCADE)
-    likes = models.ManyToManyField(
-         settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
 
     class Meta:
         ordering = ['-created_at']
