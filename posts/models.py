b9887a03003fb06bbfb61fbce5e52c4682e568fc
SamGree <sam.green7777@yahoo.com>
2025-02-11 20:01:07 +0100

    format the code for serializers file

--- posts/models.py
+++ posts/models.py
@@ -4,9 +4,10 @@ from users.models import User
 from django.conf import settings
 from cloudinary.models import CloudinaryField
 
+
 class Post(models.Model):
     """
-    Model representing a blog post, including title, description, 
+    Model representing a blog post, including title, description,
     associated user, and related likes.
     """
     title = models.CharField(max_length=100)
@@ -14,8 +15,10 @@ class Post(models.Model):
     image = CloudinaryField('image')  # Image uploaded via Cloudinary
     download_count = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
-    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
+    user = models.ForeignKey(
+        User, related_name='posts', on_delete=models.CASCADE)
+    likes = models.ManyToManyField(
+         settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
 
     class Meta:
         ordering = ['-created_at']
@@ -24,4 +27,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
--- posts/urls.py
+++ posts/urls.py
@@ -6,8 +6,8 @@ from django.urls import path
 from .views import PostListCreateView, PostDetailView, PostDownloadView, SearchView
 
 urlpatterns = [
-    path('', PostListCreateView.as_view(), name='list-create-posts'),# List all posts or create a new post
-    path('<int:post_id>', PostDetailView.as_view(), name='detail-post'), # Retrieve, update, or delete a specific post
+    path('', PostListCreateView.as_view(), name='list-create-posts'),
+    path('<int:post_id>', PostDetailView.as_view(), name='detail-post'),
     path('<int:post_id>/download', PostDownloadView.as_view(), name='post-download'),# Download a specific post
     path('search/', SearchView.as_view(), name='search'), # Perform a search across posts
 ]
