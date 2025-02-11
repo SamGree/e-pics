616d515ac00127469986ea2ab9b8c3bd7284e638
SamGree <sam.green7777@yahoo.com>
2025-02-11 19:22:58 +0100

    format code for postlikes app

--- postlikes/urls.py
+++ postlikes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostLikeView, LikedPostsView
 
 urlpatterns = [
-    path('<int:post_id>', PostLikeView.as_view(), name='post-like'),# Like a specific post
+    path('<int:post_id>', PostLikeView.as_view(), name='post-like'),
     path('', LikedPostsView.as_view(), name='post-like'),
 ]
--- postlikes/views.py
+++ postlikes/views.py
@@ -19,33 +19,34 @@ class PostLikeView(APIView):
         try:
             post = Post.objects.get(id=post_id)
         except Post.DoesNotExist:
-            return Response({'message':'Post not fount!'}, status= status.HTTP_404_NOT_FOUND)
+            return Response({'message': 'Post not found!'},
+                            status=status.HTTP_404_NOT_FOUND)
 
         if request.user in post.likes.all():
             # User unliked the post
             post.likes.remove(request.user)
             likes_count = post.likes.count()
-            post_like = PostLike.objects.get( user= request.user, post= post )
+            post_like = PostLike.objects.get(user=request.user, post=post)
             post_like.delete()
             return Response({
                 'message': 'Like removed from post.',
                 'likes_count': likes_count,
                 'is_liked': False,
-                'post_id' : post_like.post_id,
-                'user_id' : post_like.user_id,
+                'post_id': post_like.post_id,
+                'user_id': post_like.user_id,
             }, status=status.HTTP_200_OK)
         else:
-            # User liked the post 
+            # User liked the post
             post.likes.add(request.user)
             likes_count = post.likes.count()
-            post_like = PostLike.objects.create( user= request.user, post= post )
+            post_like = PostLike.objects.create(user=request.user, post=post)
             post_like.save()
             return Response({
                 'message': 'Post liked.',
                 'likes_count': likes_count,
                 'is_liked': True,
-                'post_id' : post_like.post_id,
-                'user_id' : post_like.user_id,
+                'post_id': post_like.post_id,
+                'user_id': post_like.user_id,
             }, status=status.HTTP_201_CREATED)
 
 
@@ -57,9 +58,10 @@ class LikedPostsView(APIView):
 
     def get(self, request):
         user = request.user
-        
-        liked_post_ids = PostLike.objects.filter(user=user).values_list('post', flat=True)
-        liked_posts = Post.objects.filter(id__in=liked_post_ids)
-        serializer = PostSerializer(liked_posts, many=True, context={'request': request})
-        return Response(serializer.data, status=200)
 
+        liked_post_ids = PostLike.objects.filter(
+            user=user).values_list('post', flat=True)
+        liked_posts = Post.objects.filter(id__in=liked_post_ids)
+        serializer = PostSerializer(
+            liked_posts, many=True, context={'request': request})
+        return Response(serializer.data, status=200)
